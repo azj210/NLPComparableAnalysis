4663f8c2e79279227162cc4c0b6a7ddd3c74c2e1
azj210 <61850143+azj210@users.noreply.github.com>
2020-04-14 18:53:12 -0400

    Added manufacturing sector

--- ComparableAnalysis/Main.py
+++ ComparableAnalysis/Main.py
@@ -10,14 +10,51 @@ from nltk.stem import PorterStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
 
+print("a")
+
 #Obtaining SEC docs
 def extract_docs(comp_name,cik):
-	company = edgar.Company(comp_name, cik)
-	tree = company.getAllFilings(filingType = "10-K")
-	docs = edgar.getDocuments(tree, noOfDocuments=1)
-	return docs
+  company = edgar.Company(comp_name, cik)
+  tree = company.getAllFilings(filingType = "10-K")
+  docs = edgar.getDocuments(tree, noOfDocuments=2)
+  if ("This application relies heavily on JavaScript, you" in docs[0]):
+    return docs[1]
+  else:
+    return docs[0]
 
-print('a')
+#cleaning thet document to retain only the first half and to keep a unified format for all documents
+def clean_doc(doc):
+ #splitting the doc in two since most info later is balance sheet and numbers. Also eliminate most of intro.
+  cleaned_doc = ""
+  for i in range(5000,int(len(doc)//2)):
+    cleaned_doc += doc[i]
+  keep_letters = re.sub('[^a-zA-Z]', ' ', cleaned_doc)
+  words = keep_letters.lower().split()
+  stop_words = set(stopwords.words("english"))
+  return_words =  [i for i in words if not i in stop_words]
+  return_words2 = ' '.join(return_words)
+  return return_words2
+
+#stemmer to obtain stem words
+stemmer = PorterStemmer()
+def stem_words(words_list, stemmer):
+  return [stemmer.stem(word) for word in words_list]
+
+#stemming the document
+def stemmed_doc(text):
+  tokens = nltk.word_tokenize(text)
+  stems = stem_words(tokens, stemmer)
+  stemmed = ""
+  for i in stems:
+    stemmed += i + " "
+  return stemmed
+
+#finalized doc
+def finalize_doc(doc):
+  part1 = clean_doc(doc)
+  part2 = stemmed_doc(part1)
+  #part2 = stem_words(part1, stemmer)
+  return part2
 
 
 #initial set
@@ -29,88 +66,52 @@ consumerdisc_documents = [finalize_doc(extract_docs("STARBUCKS CORP", "000082922
 manufacturing_documents = [finalize_doc(extract_docs("General Motors Co", "0001467858")),finalize_doc(extract_docs("LOCKHEED MARTIN CORP", "0000936468")),finalize_doc(extract_docs("CATERPILLAR INC", "0000018230")),finalize_doc(extract_docs("FORD MOTOR CO", "0000037996")),finalize_doc(extract_docs("RELIANCE STEEL & ALUMINUM CO", "0000861884"))]
 
 
-
-#cleaning thet document to retain only the first half and to keep a unified format for all documents
-def clean_doc(doc):
-   #splitting the doc in two since most info later is balance sheet and numbers. Also eliminate most of intro.
-   cleaned_doc = ""
-   for i in range(5000,int(len(doc)//2)):
-      cleaned_doc += doc[i]
-   keep_letters = re.sub('[^a-zA-Z]', ' ', cleaned_doc)
-   words = keep_letters.lower().split()
-   stop_words = set(stopwords.words("english"))
-   return_words =  [i for i in words if not i in stop_words]
-   return_words2 = ' '.join(return_words)
-   return return_words2
-
-#stemmer to obtain stem words
-stemmer = PorterStemmer()
-def stem_words(words_list, stemmer):
-    return [stemmer.stem(word) for word in words_list]
-
-#stemming the document
-def stemmed_doc(text):
-    tokens = nltk.word_tokenize(text)
-    stems = stem_words(tokens, stemmer)
-    stemmed = ""
-    for i in stems:
-    	stemmed += i + " "
-    return stemmed
-
-#finalized doc
-def finalize_doc(doc):
-	part1 = clean_doc(doc)
-	part2 = stemmed_doc(part1)
-	return part2
-
-
-
 tokenize = lambda doc: doc.lower().split(" ")
 
 def jaccard_similarity(query, document):
-    intersection = set(query).intersection(set(document))
-    union = set(query).union(set(document))
-    return len(intersection)/len(union)
+  intersection = set(query).intersection(set(document))
+  union = set(query).union(set(document))
+  return len(intersection)/len(union)
 
 def term_frequency(term, tokenized_document):
-    return tokenized_document.count(term)
+  return tokenized_document.count(term)
 
 def sublinear_term_frequency(term, tokenized_document):
-    count = tokenized_document.count(term)
-    if count == 0:
-        return 0
-    return 1 + math.log(count)
+  count = tokenized_document.count(term)
+  if count == 0:
+    return 0
+  return 1 + math.log(count)
 
 def augmented_term_frequency(term, tokenized_document):
-    max_count = max([term_frequency(t, tokenized_document) for t in tokenized_document])
-    return (0.5 + ((0.5 * term_frequency(term, tokenized_document))/max_count))
+  max_count = max([term_frequency(t, tokenized_document) for t in tokenized_document])
+  return (0.5 + ((0.5 * term_frequency(term, tokenized_document))/max_count))
 
 def inverse_document_frequencies(tokenized_documents):
-    idf_values = {}
-    all_tokens_set = set([item for sublist in tokenized_documents for item in sublist])
-    for tkn in all_tokens_set:
-        contains_token = map(lambda doc: tkn in doc, tokenized_documents)
-        idf_values[tkn] = 1 + math.log(len(tokenized_documents)/(sum(contains_token)))
-    return idf_values
+  idf_values = {}
+  all_tokens_set = set([item for sublist in tokenized_documents for item in sublist])
+  for tkn in all_tokens_set:
+    contains_token = map(lambda doc: tkn in doc, tokenized_documents)
+    idf_values[tkn] = 1 + math.log(len(tokenized_documents)/(sum(contains_token)))
+  return idf_values
 
 def tfidf(documents):
-    tokenized_documents = [tokenize(d) for d in documents]
-    idf = inverse_document_frequencies(tokenized_documents)
-    tfidf_documents = []
-    for document in tokenized_documents:
-        doc_tfidf = []
-        for term in idf.keys():
-            tf = sublinear_term_frequency(term, document)
-            doc_tfidf.append(tf * idf[term])
-        tfidf_documents.append(doc_tfidf)
-    return tfidf_documents
+  tokenized_documents = [tokenize(d) for d in documents]
+  idf = inverse_document_frequencies(tokenized_documents)
+  tfidf_documents = []
+  for document in tokenized_documents:
+      doc_tfidf = []
+      for term in idf.keys():
+        tf = sublinear_term_frequency(term, document)
+        doc_tfidf.append(tf * idf[term])
+      tfidf_documents.append(doc_tfidf)
+  return tfidf_documents
 
 def cosine_similarity(vector1, vector2):
-    dot_product = sum(p*q for p,q in zip(vector1, vector2))
-    magnitude = math.sqrt(sum([val**2 for val in vector1])) * math.sqrt(sum([val**2 for val in vector2]))
-    if not magnitude:
-        return 0
-    return dot_product/magnitude
+  dot_product = sum(p*q for p,q in zip(vector1, vector2))
+  magnitude = math.sqrt(sum([val**2 for val in vector1])) * math.sqrt(sum([val**2 for val in vector2]))
+  if not magnitude:
+    return 0
+  return dot_product/magnitude
 
 
 #development set
@@ -119,53 +120,53 @@ bio_Dev = [finalize_doc(extract_docs("LILLY ELI & CO", "0000059478")), finalize_
 finance_Dev = [finalize_doc(extract_docs("SVB FINANCIAL GROUP", "0000719739")),  finalize_doc(extract_docs("JPMORGAN CHASE & CO", "0000019617")), finalize_doc(extract_docs("BANK OF AMERICA CORP /DE/", "0000070858"))]
 energy_Dev = [finalize_doc(extract_docs("VALERO ENERGY CORP/TX", "0001035002")), finalize_doc(extract_docs("EOG RESOURCES INC", "0000821189")), finalize_doc(extract_docs("OCCIDENTAL PETROLEUM CORP /DE/", "0000797468"))]
 consumerdisc_Dev = [finalize_doc(extract_docs("NIKE INC", "0000320187")), finalize_doc(extract_docs("TJX COMPANIES INC /DE/", "0000109198")), finalize_doc(extract_docs("MCDONALDS CORP", "0000063908"))]      
+manufacturing_Dev = [finalize_doc(extract_docs("BOEING CO", "0000012927")), finalize_doc(extract_docs("GOODYEAR TIRE & RUBBER CO /OH/", "0000042582")) ,finalize_doc(extract_docs("LEAR CORP", "0000842162"))]
 
-development_docs = [it_documents + it_Dev, bio_documents + bio_Dev, finance_documents + finance_Dev, energy_documents + energy_Dev, consumerdisc_documents + consumerdisc_Dev]
+development_docs = [it_documents + it_Dev, bio_documents + bio_Dev, finance_documents + finance_Dev, energy_documents + energy_Dev, consumerdisc_documents + consumerdisc_Dev, manufacturing_documents + manufacturing_Dev]
 docsPerSector = len(development_docs[0])
 
 similarities = {"technology":[],"biopharmaceuticals":[],"finance":[],"energy":[],"consumer_discretionary":[], "manufacturing":[]}
 sim_list = list(similarities.keys())
 
 
+#6 sectors total
+#8 docs in each sector 
 def generate_cosine_similarities(doc,development_docs,similarity):
   #test, change all run_docs back to development docs
-	run_docs = development_docs.copy()
-	finalized_doc = finalize_doc(doc)
-	#add test doc in for testing
-	for i in range(len(run_docs)):
-		run_docs[i].append(finalized_doc)
-	#tfidf vectorization
-	for i in range(len(run_docs)):
-		temp_tfidf = tfidf(run_docs[i])
-		average_cosim = 0
-		for j in range(docsPerSector):
-			hold = cosine_similarity(temp_tfidf[j],temp_tfidf[docsPerSector])
-			average_cosim += hold
-			similarity[sim_list[i]].append(hold)
-		similarity[sim_list[i]].append(average_cosim/docsPerSector)
+  run_docs = development_docs.copy()
+  finalized_doc = finalize_doc(doc)
+  #add test doc in for testing
+  for i in range(len(run_docs)):
+    run_docs[i].append(finalized_doc)
+  #tfidf vectorization
+  for i in range(len(run_docs)):
+    temp_tfidf = tfidf(run_docs[i])
+    average_cosim = 0
+    for j in range(docsPerSector):
+      hold = cosine_similarity(temp_tfidf[j],temp_tfidf[docsPerSector])
+      average_cosim += hold
+      similarity[sim_list[i]].append(hold)
+    similarity[sim_list[i]].append(average_cosim/docsPerSector)
 
 def sector_ranking(sims):
-	#tracking the sector with most similarity
-	greatest = [0, 0]
-	for i in sims.keys():
-		if sims[i][docsPerSector] > greatest[1]:
-			greatest[0] = i
-			greatest[1] = sims[i][docsPerSector]
-	#clear dictionary
-	sims = sims.fromkeys(sims,[])
-	return greatest
+  #tracking the sector with most similarity
+  greatest = [0, 0]
+  for i in sims.keys():
+    if sims[i][docsPerSector] > greatest[1]:
+      greatest[0] = i
+      greatest[1] = sims[i][docsPerSector]
+  #clear dictionary
+  sims = sims.fromkeys(sims,[])
+  return greatest
 
 def gen_output(td,d,s):
-	generate_cosine_similarities(td,d,s)
-	#display entire dictionary of similarities
-	print(s)
-	return(sector_ranking(s))
+  generate_cosine_similarities(td,d,s)
+  #display entire dictionary of similarities
+  print(s)
+  return(sector_ranking(s))
 
 
-print(gen_output(extract_docs("AMAZON COM INC", "0001018724"), development_docs, similarities))
-print(gen_output(extract_docs("ADOBE INC.", "0000796343"), development_docs, similarities))
-print(gen_output(extract_docs("Lyft, Inc.", "0001759509"), development_docs, similarities))
-
+print(gen_output(extract_docs("Tesla, Inc.", "0001318605"), development_docs, similarities))
 
 
 
